a679db8b43392e8d523c8b0a61f397fb4a069fb2
yuriikovtik-cpu <yuriikovtik@gmail.com>
2026-02-10 21:42:18 +0200

    Add files via upload

--- Playwright/PlaywrightProject/PlaywrightProject/urls.py
+++ Playwright/PlaywrightProject/PlaywrightProject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('ParseApp.urls')),
+    path('', include('modules.urls')),
     path('admin/', admin.site.urls),
 ]
