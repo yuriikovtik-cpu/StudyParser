ddcb0747f33ff2759a87d0b10e9e5cbb232adae6
yuriikovtik-cpu <yuriikovtik@gmail.com>
2026-02-10 19:45:05 +0200

    Add files via upload

---
+++ Test/RequestsProject/modules/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Tool(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+
+    memory = models.CharField(max_length=50, null=True, blank=True)
+    price = models.CharField(max_length=50, null=True, blank=True)
+
+    img_url = models.URLField(null=True, blank=True)
+
+    productCode = models.CharField(max_length=100, null=True, blank=True)
+    comment = models.CharField(max_length=100, null=True, blank=True)
+
+    screen_diagonal = models.TextField(null=True, blank=True)
+    screen_resolution = models.TextField(null=True, blank=True)
+    specifications = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
